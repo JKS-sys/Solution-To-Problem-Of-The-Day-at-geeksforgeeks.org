ab38374813044a44acde12a56fca9dd314a62a23
Jagadeesh Kumar   S <jagadeesh_2k17@proton.me>
2026-04-08 20:53:03 +0530

    Add solution for segregating 0s and 1s in an array

---
+++ wed, 08 apr 2026.js
@@ -0,0 +1,19 @@
+class Solution {
+    segregate0and1(arr) {
+        let left = 0;
+        let right = arr.length - 1;
+
+        while (left < right) {
+            if (arr[left] === 0) {
+                left++;
+            } else if (arr[right] === 1) {
+                right--;
+            } else {
+                // arr[left] is 1 and arr[right] is 0
+                [arr[left], arr[right]] = [arr[right], arr[left]];
+                left++;
+                right--;
+            }
+        }
+    }
+}
